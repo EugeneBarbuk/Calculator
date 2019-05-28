a230e48940773b6f739bee16f300d0c60dac0195
EugeneBarbuk <Barbuk.Eugene@gmail.com>
2019-05-28 16:11:29 +0300

    Add screenshot capture

--- src/test/java/by/epam/barbuk/lab/Runner.java
+++ src/test/java/by/epam/barbuk/lab/Runner.java
@@ -3,12 +3,18 @@ package by.epam.barbuk.lab;
 import org.testng.TestNG;
 import org.testng.xml.XmlSuite;
 
+import javax.imageio.ImageIO;
+import java.awt.Robot;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Runner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AWTException, IOException {
         TestNG testNg = new TestNG();
 
         XmlSuite suite = new XmlSuite();
@@ -19,6 +25,9 @@ public class Runner {
 
         testNg.setXmlSuites(suites);
         testNg.run();
+
+        BufferedImage image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+        ImageIO.write(image, "png", new File("./target/screen.png"));
     }
 }
 
